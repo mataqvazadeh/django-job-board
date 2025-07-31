5a6b43138ad9a73deb157d309935044df069fbb5
Mohammad Ali Taqvazadeh <m.a.taqvazadeh@gmail.com>
2025-08-01 00:32:55 +0330

    Fix views problems

--- jobs/urls.py
+++ jobs/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.JobListView),
-    path('<int:id>/', views.JobDetailView),
+    path('', views.JobListView.as_view()),
+    path('<int:id>/', views.JobDetailView.as_view()),
 ]
